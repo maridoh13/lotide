54988e64cbdf136ea13b09c6b9af8b7a3151e3ee
maridoh13 <maridoh@gmail.com>
2020-02-04 03:23:42 +0000

    assert console

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log("😀" + " Assertion Passed: " + actual + " === " + expected);
+  }
+  
+  return console.log("❌ " + "Assertion Failed: " + actual + " !== " + expected);
+  
+};
+
+// TEST CODE;
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Mariana", "Mariana");
+assertEqual(1, "1");
