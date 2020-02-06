8a2649235a20dfb57081bea59513bccf10102934
maridoh13 <maridoh@gmail.com>
2020-02-06 02:48:29 +0000

    updated size comparison

--- eqArrays.js
+++ eqArrays.js
@@ -7,16 +7,21 @@ const assertEqual = function(actual, expected) {
   
 };
 
-eqArrays = function(arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   var result = true;
-  
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      result = false;
+  if (arr1.length === arr2.length) {
+    for (var i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        result = false;
+      }
     }
+  } else {
+    result = false;
   }
   return result;
 }
+
+console.log(eqArrays([1, 2], [1, 2 ,3]));
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
