d33bb6268e7db8e7754814f7f21ad3089e178646
maridoh13 <maridoh@gmail.com>
2020-02-06 04:33:41 +0000

    updated

--- eqObjects.js
+++ eqObjects.js
@@ -28,42 +28,36 @@ const eqObjects = function(object1, object2) {
   let arr2 = Object.keys(object2);
 
   if (arr1.length === arr2.length) {
-
     for (const element of arr1) {
-      
       if (Array.isArray(object1[element]) && Array.isArray(object2[element])) {
-        result = eqArrays(object1[element], object2[element]);
-      }
-      else{ 
-        if (object1[element] !== object2[element]) {
-          result = false;
+        if (! eqArrays(object1[element], object2[element])) {
+          return false;
         }
-        else {
-          result = true;
+      } else{ 
+        if (object1[element] !== object2[element]) {
+          return false;
         }
       }
     }  
-
   }
   else {
-    result = false;
+    return false;
   }
-
-  return result; //true or false
+  return true; //true or false
 };
 
 
 // TEST
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
+const ab = { a: "1", b: "2"};
+const ba = { b: "2", a: 1};
 console.log(eqObjects(ab, ba)); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+// console.log(eqObjects(ab, abc)); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+// console.log(eqObjects(cd, dc)); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+// console.log(eqObjects(cd, cd2)); // => false
