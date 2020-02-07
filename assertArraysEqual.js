7b28725e6a4146786d8e13a1735e8c0ac44e973f
maridoh13 <maridoh@gmail.com>
2020-02-07 02:02:05 +0000

    update

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,10 +12,10 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2) === true) {
-    return console.log("The arrays are the same!");
+    return console.log("The arrays are the same! 😄😄😄");
   }
   else {
-    return console.log("The arrays are not the same!");
+    return console.log("The arrays are not the same! 🥺🥺🥺");
   }
 }
 
