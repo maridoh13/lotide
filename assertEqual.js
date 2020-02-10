0c2ca6b7fb44aec63bc197fe327bae52af494dad
maridoh13 <maridoh@gmail.com>
2020-02-10 20:43:55 +0000

    updated

--- assertEqual.js
+++ assertEqual.js
@@ -7,10 +7,4 @@ const assertEqual = function(actual, expected) {
   return console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE;
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("Mariana", "Mariana");
-// assertEqual(1, "1");
-
 module.exports = assertEqual;
--- head.js
+++ head.js
@@ -4,11 +4,4 @@ const head = function(array) {
   return array[0];
 };
 
-//TEST CODE
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// console.log(head([2, 7, 8]));
-// console.log(head([]));
-// console.log(head([5]));
-
 module.exports = head;
