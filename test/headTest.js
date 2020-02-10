36fa18c71c529fdf982bfbb686bd904b58d91311
maridoh13 <maridoh@gmail.com>
2020-02-10 21:37:23 +0000

    updated

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,25 @@
-//TEST CODE
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// console.log(head([2, 7, 8]));
-// console.log(head([]));
-// console.log(head([5]));
-
+const assert = require('chai').assert;
 const head = require('../head');
 
+describe("#head function", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns the value if there is only one item in array: '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns 'Hello' for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns undefined when input is an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,22 @@
-//Test Cases: Check the original array
-// console.log(tail([1, 2, 3, 4, 5]));
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// console.log(result);
-// assertEqual(result.length, 2); // ensure we get back two elements
-// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
 const tail = require('../tail');
+const assert = require('chai').assert;
 
+describe("#tail function", () => {
+  it("returns ['Lighthouse', 'Labs'] from ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs'])
+  });
+
+  it("the original array should be not modified", () => {
+    const input = [1, 2, 3];
+    tail(input);
+    assert.deepEqual(input, [1, 2, 3]);
+  });
+
+  it("an array with one element should return an empty array", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+
+  it("empty array returns empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
