978ef9beb2a438057a53bdc1f1bc2852db59a7b4
maridoh13 <maridoh@gmail.com>
2020-02-10 20:47:39 +0000

    update

--- tail.js
+++ tail.js
@@ -5,17 +5,4 @@ const tail = function tail(array) {
   return newArray;
 };
 
-//Test Cases: Check the original array
-// console.log(tail([1, 2, 3, 4, 5]));
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// console.log(result);
-// assertEqual(result.length, 2); // ensure we get back two elements
-// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
 module.exports = tail;
